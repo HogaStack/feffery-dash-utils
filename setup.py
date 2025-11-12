1acfb0c62f4c5b15e475d6c5d86d3ad40a7dd340
insistence <3055204202@qq.com>
2025-11-12 15:33:03 +0800

    chore: 完善项目构建

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
-import io
 from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+with open('./requirements/install.txt', 'r', encoding='utf-8') as fh:
+    requirements = [
+        line.strip() for line in fh if line.strip() and not line.startswith('#')
+    ]
 
 setup(
     name='feffery_dash_utils',
     version='0.2.6',
     author_email='fefferypzy@gmail.com',
-    homepage='https://github.com/CNFeffery/feffery-dash-utils',
+    homepage='https://github.com/HogaStack/feffery-dash-utils',
     author='CNFeffery <fefferypzy@gmail.com>',
     packages=find_packages(),
     license='MIT',
     description='A series of tool functions to assist Dash application development.',
-    long_description=io.open(
-        'README.md', encoding='utf-8'
-    ).read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     classifiers=[
         'Framework :: Dash',
     ],
-    url='https://github.com/CNFeffery/feffery-dash-utils',
-    python_requires=">=3.8",
-    install_requires=['cssutils', 'packaging'],
+    url='https://github.com/HogaStack/feffery-dash-utils',
+    python_requires='>=3.8',
+    install_requires=requirements,
 )
